df0f8e413d7dd87b2d9709bca5673f7c9bf35849
Nick Robson <robson.nicholas1@gmail.com>
2017-06-20 10:44:43 +1000

    TEMPORARY: disable service worker

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
+
+
+
+  //
+  // DISABLE THE SERVICE WORKER
+  //
+  return; // REMOVE THIS TO ENABLE SERVICE WORKER
+  //
+  // DISABLE THE SERVICE WORKER
+  //
+
+
+
+
   // Check to make sure service workers are supported in the current browser,
   // and that the current page is accessed from a secure origin. Using a
   // service worker from an insecure origin will trigger JS console errors.
