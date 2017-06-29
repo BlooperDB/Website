095a8646e5facba5df6aad6d609c6ce10b20e176
Vilsol <vilsol2000@gmail.com>
2017-06-29 05:52:24 +0300

    Change card layout

--- src/components/partials/BlueprintCard.vue
+++ src/components/partials/BlueprintCard.vue
@@ -1,23 +1,28 @@
 <template>
   <md-card md-with-hover class="blueprint-card" style="position: relative; width: 100%;">
     <router-link :to="`/view/${blueprint.id}`">
-      <md-card-media>
-        <img src="../../assets/img/logo.svg" style="padding: 15px; background-color: #e44;">
-      </md-card-media>
+      <md-card-media-cover md-text-scrim>
+        <md-card-media md-ratio="1:1">
+          <img src="../../assets/img/logo.svg" style="padding: 15px; background-color: #e44;">
+        </md-card-media>
 
-      <md-card-header style="margin-bottom: 40px;">
-        <div class="md-title">{{ blueprint.name }}</div>
-        <div class="md-subhead">{{ blueprint.description }}</div>
-      </md-card-header>
+        <md-card-area>
+          <md-card-header>
+            <div class="md-title">{{ blueprint.name }}</div>
+            <div class="md-subhead">{{ blueprint.description }}</div>
+          </md-card-header>
 
-      <md-card-actions class="card-tags" v-if="blueprint.tags.length > 0">
-        <div>
-          <md-chip
-            v-for="(tag, idx) in blueprint.tags"
-            :key="idx"
-            style="margin-left: 5px">{{ tag }}</md-chip>
-        </div>
-      </md-card-actions>
+          <md-card-actions class="card-tags" v-if="blueprint.tags.length > 0">
+            <div>
+              <md-chip
+                v-for="(tag, idx) in blueprint.tags"
+                :key="idx"
+                style="margin-left: 5px">{{ tag }}
+              </md-chip>
+            </div>
+          </md-card-actions>
+        </md-card-area>
+      </md-card-media-cover>
     </router-link>
   </md-card>
 </template>
@@ -70,14 +75,10 @@
       text-decoration: none;
     }
   }
+
   .md-title, .md-subhead {
     text-overflow: ellipsis;
     overflow: hidden;
     white-space: nowrap;
-  }
-  .card-tags {
-    position: absolute;
-    bottom: 0;
-    right: 0;
   }
 </style>
